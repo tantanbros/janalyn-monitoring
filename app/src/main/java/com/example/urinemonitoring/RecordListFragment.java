3552d40e86037648c761425f26cac35f67c8b5c4
tantanbros <tantansoftdevinc@gmail.com>
2020-09-05 12:41:51 +0800

    Update write external storage permissions and change downloads directory to DCIM

--- app/src/main/java/com/example/urinemonitoring/RecordListFragment.java
+++ app/src/main/java/com/example/urinemonitoring/RecordListFragment.java
@@ -1,8 +1,10 @@
 package com.example.urinemonitoring;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
@@ -14,7 +16,12 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.Observer;
@@ -89,18 +96,61 @@ public class RecordListFragment extends Fragment {
         });
     }
 
+    interface Downloadable {
+        void run();
+    }
+
     private void showDialog(final LifecycleOwner owner) {
+
         LayoutInflater inflater = getActivity().getLayoutInflater();
         View view = inflater.inflate(R.layout.dialog_filename, null);
         TextView txtFileName = view.findViewById(R.id.txtFileName);
 
         // create the parent directories for each device
-        final File path = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), String.format("Device_%s", patient.getDevice()));
+        final File path = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM), String.format("Device_%s", patient.getDevice()));
         if(!path.exists()) {
             path.mkdirs();
         }
         txtFileName.setText(path.getPath());
         Log.d(TAG, "Download Path: " + path.getAbsolutePath());
+
+        final Downloadable d = new Downloadable() {
+            @Override
+            public void run() {
+                // Get the records from db and write to files
+                recordViewModel.getRecordsByPatient(patient.getId()).observe(owner, new Observer<List<Record>>() {
+                    @Override
+                    public void onChanged(List<Record> records) {
+                        Log.d("DownloadDialog", "Download: " + records.size() + " records");
+                        try {
+                            int i = 0;
+                            for(Record r: records) {
+                                // create the file per record
+                                String filename = String.format("Record %d_%s.txt", i, r.getRecordTime());
+                                File file = new File(path, filename);
+                                if(!file.getParentFile().exists()) {
+                                    file.getParentFile().mkdirs();
+                                }
+                                if(!file.exists()) {
+                                    file.createNewFile();
+                                    Log.d(TAG, "Download Record: " + filename);
+
+                                    FileOutputStream stream = new FileOutputStream(file);
+                                    stream.write(r.toString().getBytes());
+                                    stream.close();
+                                }
+                                i++;
+                            }
+                            Toast.makeText(getContext(), "Records Downloaded", Toast.LENGTH_SHORT).show();
+                        }
+                        catch (IOException e) {
+                            Log.e("Exception", "File write failed: " + e.toString());
+                        }
+                        recordViewModel.getRecordsByPatient(patient.getId()).removeObserver(this);
+                    }
+                });
+            }
+        };
 
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
@@ -108,37 +158,40 @@ public class RecordListFragment extends Fragment {
                 .setView(view)
                 .setPositiveButton("Download", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        recordViewModel.getRecordsByPatient(patient.getId()).observe(owner, new Observer<List<Record>>() {
-                            @Override
-                            public void onChanged(List<Record> records) {
-                                Log.d("DownloadDialog", "Download: " + records.size() + " records");
-                                try {
-                                    int i = 0;
-                                    for(Record r: records) {
-                                        // create the file per record
-                                        String filename = String.format("Record %d_%s.txt", i, r.getRecordTime());
-                                        File file = new File(path, filename);
-                                        if(!file.getParentFile().exists()) {
-                                            file.getParentFile().mkdirs();
+                        // Runtime permissions
+                        ActivityResultLauncher<String> requestPermissionLauncher =
+                                getActivity().registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+                                    @Override
+                                    public void onActivityResult(Boolean isGranted) {
+                                        if (isGranted) {
+                                            // Permission is granted. Continue the action or workflow in your
+                                            // app.
+                                            Log.d(TAG, "requestPermissionLauncher - PERMISSION_GRANTED - Downloading Records");
+                                            d.run();
+                                        } else {
+                                            // Explain to the user that the feature is unavailable because the
+                                            // features requires a permission that the user has denied. At the
+                                            // same time, respect the user's decision. Don't link to system
+                                            // settings in an effort to convince the user to change their
+                                            // decision.
+                                            Log.d(TAG, "requestPermissionLauncher - PERMISSION_DENIED - Feature Unavailable");
+                                            Toast.makeText(RecordListFragment.this.getContext(), "The feature unavailable because you denied it", Toast.LENGTH_SHORT).show();
                                         }
-                                        if(!file.exists()) {
-                                            file.createNewFile();
-                                            Log.d(TAG, "Download Record: " + filename);
-
-                                            FileOutputStream stream = new FileOutputStream(file);
-                                            stream.write(r.toString().getBytes());
-                                            stream.close();
-                                        }
-                                        i++;
                                     }
-                                    Toast.makeText(getContext(), "Records Downloaded", Toast.LENGTH_SHORT).show();
-                                }
-                                catch (IOException e) {
-                                    Log.e("Exception", "File write failed: " + e.toString());
-                                }
-                                recordViewModel.getRecordsByPatient(patient.getId()).removeObserver(this);
-                            }
-                        });
+                                });
+
+//                        // check for permissions
+                        if (ContextCompat.checkSelfPermission(
+                                RecordListFragment.this.getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                            // You can use the API that requires the permission.
+                            Log.d(TAG, "checkSelfPermission - PERMISSION_GRANTED - Downloading Records");
+                            d.run();
+                        }  else {
+                            // You can directly ask for the permission.
+                            // The registered ActivityResultCallback gets the result of this request.
+                            Log.d(TAG, "checkSelfPermission - PERMISSION_DENIED - Asking for Permission");
+                            requestPermissionLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                        }
                     }
                 })
                 .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
